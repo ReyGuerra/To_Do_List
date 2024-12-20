7808f23205c1669a02fb1316df22bb968cbdcab1
Rey <Reyreyguerra14@gmail.com>
2024-12-20 16:16:06 -0600

    Added persistant data, so data will stay even after refresh

--- reactjs_todolist/src/App.jsx
+++ reactjs_todolist/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import ToDoInput from "./components/ToDoInput"
 import ToDoList from "./components/ToDoList"
 
@@ -9,15 +9,22 @@ function App() {
 
   const [todoValue, setTodoValue] = useState('')
 
+  function persistData(newList){
+    localStorage.setItem('todos', JSON.stringify({todos: newList}))
+  }
+
+
   function handleAddTodos(newTodo){
-    const newToDoList = [...todos, newTodo]
-    setTodos(newToDoList)
+    const newTodoList = [...todos, newTodo]
+    persistData(newTodoList)
+    setTodos(newTodoList)
   }
 
   function handleDeleteTodo(index){
     const newTodoList = todos.filter((todo, todoIndex) => {
       return todoIndex !== index
     })
+    persistData(newTodoList)
     setTodos(newTodoList)
   }
 
@@ -26,8 +33,25 @@ function App() {
     setTodoValue(valToEdit)
   
     handleDeleteTodo(index)
-
   }
+
+  useEffect(() => {
+    if(!localStorage){
+      return
+    }
+
+    let localTodos = localStorage.getItem('todos')
+    if (!localTodos){
+      return
+    }
+
+    localTodos = JSON.parse(localTodos).todos
+    setTodos(localTodos)
+    
+  }, [])
+
+
+
 
   return (
     <>
